0cdaf0d9a51d6964f01314e792241ceb4585dfbd
Leocadio G. Casado <leo@ual.es>
2021-06-16 20:07:26 +0200

    Leo

--- test_examples/MS-SevTasks.py
+++ test_examples/MS-SevTasks.py
@@ -38,14 +38,14 @@ TagDiv=3
 TagRes=4
 TagEnd=5
 
-#Define set ooerations mumber/function/symbol
+#Define set operations mumber/function/symbol
 OperNumb=[TagAdd,TagSub,TagMul,TagDiv]
 Operfunc={TagAdd:fAdd, TagSub:fSub, TagMul:fMul, TagDiv:fDiv}
 OperSymb={TagAdd:"+", TagSub:"-", TagMul:"*", TagDiv:"/"}
 
 
 if rankMPI == 0: 	#I am master
-	ListSize=100	#size of the list.
+	ListSize=100	#size of the list of operations
 	List=[]
 
 	#Generate a list of [a,b,oper,res] -> a oper b = res.
@@ -55,31 +55,31 @@ if rankMPI == 0: 	#I am master
 	print("----------------------------------")
 	sys.stdout.flush()
 
-	NSend=0		#Counter of elements of the list sent
+	NSend=0		#Sent operations to slaves from the List
 	PendRec=0	#How many receives are pending.
 
 	#Feed slaves 
 	SlaveId=1;
 	while PendRec<sizeMPI-1 and NSend<ListSize:
-		dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master NSend,a,b
-		commMPI.send(dataMS,dest=SlaveId,tag=List[NSend][2]) #tag shows the oper to do "a oper b"
+		dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master: (NSend,a,b)
+		commMPI.send(dataMS,dest=SlaveId,tag=List[NSend][2]) #tag shows the oper to do (a oper b)
 		NSend+=1
 		PendRec+=1
 		SlaveId+=1
 
 	#receive from slave and feed it again
 	while PendRec:
-		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagRes) #data from slave i,res,rank
+		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagRes) #data from slave (i,res,rank)
 		PendRec-=1
 		List[dataSL[0]][3]=dataSL[1]
 		if NSend<ListSize:
-			dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master NSend,a,b
-			commMPI.send(dataMS,dest=dataSL[2],tag=List[NSend][2])
+			dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master (NSend,a,b)
+			commMPI.send(dataMS,dest=dataSL[2],tag=List[NSend][2]) #tag shows the oper to do (a oper b)
 			NSend+=1
 			PendRec+=1
 
 
-	#All work done -> sent a terminate message
+	#All work done -> sent a terminate message to all workers
 	for i in range(1,sizeMPI):
 		sys.stdout.flush()
 		commMPI.send(0,dest=i,tag=TagEnd)
@@ -95,7 +95,7 @@ else: #I am a slave
 		#print(dataMS[1],OperSymb[tag],dataMS[2])
 		#sys.stdout.flush()
 		result=Operfunc[tag](dataMS[1],dataMS[2]) #fXxx(a,b)
-		dataSL=[dataMS[0],result,rankMPI]	#data from slave: i,res,rank
+		dataSL=[dataMS[0],result,rankMPI]	#data from slave: (i,res,rank)
 		commMPI.send(dataSL,dest=0,tag=TagRes)
 
 
