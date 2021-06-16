b5db99c7dfd7fde9c93b8822278486d2b9a4cc6b
Leocadio G. Casado <leo@ual.es>
2021-06-16 20:03:00 +0200

    Leo

--- test_examples/MS-SevTasks.py
+++ test_examples/MS-SevTasks.py
@@ -26,63 +26,81 @@ rankMPI = MPI.COMM_WORLD.Get_rank()
 nameMPI = MPI.Get_processor_name()
 sizeMPI = MPI.COMM_WORLD.Get_size()
 
-ListSize=10	#size of the list.
-if ListSize<sizeMPI-1:
-	if rankMPI==0:
-		print("List size < #slaves") #just the master print
-	sys.exit() #all processes do the exit
+#print("Process",rankMPI,"/",sizeMPI,"in",nameMPI)
+#sys.stdout.flush()		#to flush all queued prints.
 
+
+#Define tag values depending of the function to perform
 TagAdd=0
 TagSub=1
 TagMul=2
 TagDiv=3
-TagResult=4
+TagRes=4
 TagEnd=5
+
+#Define set ooerations mumber/function/symbol
 OperNumb=[TagAdd,TagSub,TagMul,TagDiv]
 Operfunc={TagAdd:fAdd, TagSub:fSub, TagMul:fMul, TagDiv:fDiv}
 OperSymb={TagAdd:"+", TagSub:"-", TagMul:"*", TagDiv:"/"}
 
 
 if rankMPI == 0: 	#I am master
+	ListSize=100	#size of the list.
 	List=[]
 
+	#Generate a list of [a,b,oper,res] -> a oper b = res.
 	for i in range (ListSize):
-		List.append([i,i+1,random.choice(OperNumb),None]) #a,b,oper,res	
+		List.append([i,i+1,random.choice(OperNumb),None])
+		print(i,":",List[i][0],OperSymb[List[i][2]],List[i][1],"=",List[i][3])
+	print("----------------------------------")
+	sys.stdout.flush()
+
+	NSend=0		#Counter of elements of the list sent
+	PendRec=0	#How many receives are pending.
 
 	#Feed slaves 
-	for i in range(0,sizeMPI-1):
-		dataMS=[i,List[i][0],List[i][1]]
-		commMPI.send(dataMS,dest=i+1,tag=List[i][2])
+	SlaveId=1;
+	while PendRec<sizeMPI-1 and NSend<ListSize:
+		dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master NSend,a,b
+		commMPI.send(dataMS,dest=SlaveId,tag=List[NSend][2]) #tag shows the oper to do "a oper b"
+		NSend+=1
+		PendRec+=1
+		SlaveId+=1
 
-	#receive from slaves and feed again
-	for i in range(sizeMPI-1,ListSize):
-		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagResult)
+	#receive from slave and feed it again
+	while PendRec:
+		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagRes) #data from slave i,res,rank
+		PendRec-=1
 		List[dataSL[0]][3]=dataSL[1]
-		dataMS=[i,List[i][0],List[i][1]]
-		commMPI.send(dataMS,dest=dataSL[2],tag=List[i][2])
+		if NSend<ListSize:
+			dataMS=[NSend,List[NSend][0],List[NSend][1]]	#data from Master NSend,a,b
+			commMPI.send(dataMS,dest=dataSL[2],tag=List[NSend][2])
+			NSend+=1
+			PendRec+=1
 
- 	#receive from pending slaves: all slaves have send back and sent terminate 
-	for i in range(0,sizeMPI-1):       		
-		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagResult)
-		List[data[0]][3]=data[1]	
-		#send exit message to all workers
-		commMPI.send(0,dest=dataSL[2],tag=List[i][2])
-	
+
+	#All work done -> sent a terminate message
+	for i in range(1,sizeMPI):
+		sys.stdout.flush()
+		commMPI.send(0,dest=i,tag=TagEnd)
 
 else: #I am a slave
-	status = MPI.Status()
-	dataMS=commMPI.recv(source=0,tag=MPI.ANY_TAG,status=status)
-	tag=status.Get_tag()
-	if tag==TagEnd:
-		sys.exit()
-	result=Operfunc[tag](dataMS[1],dataMS[2]) #fXxx(a,b)
-	print(dataMS[1],OperSymb[tag],dataMS[2],"=",result)
-	dataSL=[dataMS[0],result,rankMPI]
-	commMPI.send(dataSL,dest=0,tag=TagResult)
+	tag=TagRes		#Just to get into the while
+	while tag!=TagEnd:
+		status = MPI.Status()
+		dataMS=commMPI.recv(source=0,tag=MPI.ANY_TAG,status=status) #data from Master NSend,a,b
+		tag=status.Get_tag()
+		if tag==TagEnd:
+			sys.exit()
+		#print(dataMS[1],OperSymb[tag],dataMS[2])
+		#sys.stdout.flush()
+		result=Operfunc[tag](dataMS[1],dataMS[2]) #fXxx(a,b)
+		dataSL=[dataMS[0],result,rankMPI]	#data from slave: i,res,rank
+		commMPI.send(dataSL,dest=0,tag=TagRes)
 
 
 if rankMPI == 0:
-	for i in rangue(ListSize):
-		print(List[1],OperSymb[List[3]],List[2],"=",List[4])
+	for i in range(ListSize):
+		print(i,":",List[i][0],OperSymb[List[i][2]],List[i][1],"=",List[i][3])
 
 sys.exit() 
