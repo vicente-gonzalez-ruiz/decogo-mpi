cb044ccbd43ab5b513aa892a0aa1a554c0e12cb9
Leocadio G. Casado <leo@ual.es>
2021-06-16 14:58:34 +0200

    Leo

--- test_examples/MS-SevTasks.py
+++ test_examples/MS-SevTasks.py
@@ -1,48 +1,88 @@
 from mpi4py import MPI
 import random
+import sys
 
+#-------------
 def fAdd(a,b):
 	return a+b
 
+#-------------
 def fSub(a,b):
 	return a-b
 
+#-------------
 def fMul(a,b):
 	return a*b
- 
+
+#------------- 
 def fDiv(a,b):
 	return a/b
 
 
-comm = MPI.COMM_WORLD
-rank = MPI.COMM_WORLD.Get_rank()
-name = MPI.Get_processor_name()
-size = MPI.COMM_WORLD.Get_size()
+#-------------
+#-------------
+commMPI = MPI.COMM_WORLD
+rankMPI = MPI.COMM_WORLD.Get_rank()
+nameMPI = MPI.Get_processor_name()
+sizeMPI = MPI.COMM_WORLD.Get_size()
+
+ListSize=10	#size of the list.
+if ListSize<sizeMPI-1:
+	if rankMPI==0:
+		print("List size < #slaves") #just the master print
+	sys.exit() #all processes do the exit
 
 TagAdd=0
 TagSub=1
 TagMul=2
 TagDiv=3
+TagResult=4
+TagEnd=5
 OperNumb=[TagAdd,TagSub,TagMul,TagDiv]
 Operfunc={TagAdd:fAdd, TagSub:fSub, TagMul:fMul, TagDiv:fDiv}
 OperSymb={TagAdd:"+", TagSub:"-", TagMul:"*", TagDiv:"/"}
 
 
-if rank == 0: 	#I am master
-	ListSize=1000	#size of the list.
+if rankMPI == 0: 	#I am master
 	List=[]
 
 	for i in range (ListSize):
 		List.append([i,i+1,random.choice(OperNumb),None]) #a,b,oper,res	
 
-	for i in range(0,size-1):
-		data=[List[i][0],List[i][1]]
-		print(List[i][2])
-		comm.send(data,dest=i+1,tag=List[i][2])
+	#Feed slaves 
+	for i in range(0,sizeMPI-1):
+		dataMS=[i,List[i][0],List[i][1]]
+		commMPI.send(dataMS,dest=i+1,tag=List[i][2])
+
+	#receive from slaves and feed again
+	for i in range(sizeMPI-1,ListSize):
+		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagResult)
+		List[dataSL[0]][3]=dataSL[1]
+		dataMS=[i,List[i][0],List[i][1]]
+		commMPI.send(dataMS,dest=dataSL[2],tag=List[i][2])
+
+ 	#receive from pending slaves: all slaves have send back and sent terminate 
+	for i in range(0,sizeMPI-1):       		
+		dataSL=commMPI.recv(source=MPI.ANY_SOURCE,tag=TagResult)
+		List[data[0]][3]=data[1]	
+		#send exit message to all workers
+		commMPI.send(0,dest=dataSL[2],tag=List[i][2])
+	
 
 else: #I am a slave
 	status = MPI.Status()
-	data=comm.recv(source=0,tag=MPI.ANY_TAG,status=status)
+	dataMS=commMPI.recv(source=0,tag=MPI.ANY_TAG,status=status)
 	tag=status.Get_tag()
-	result=Operfunc[tag](data[0],data[1]) #fXxx(a,b)
-	print(data[0],OperSymb[tag],data[1],"=",result) 
+	if tag==TagEnd:
+		sys.exit()
+	result=Operfunc[tag](dataMS[1],dataMS[2]) #fXxx(a,b)
+	print(dataMS[1],OperSymb[tag],dataMS[2],"=",result)
+	dataSL=[dataMS[0],result,rankMPI]
+	commMPI.send(dataSL,dest=0,tag=TagResult)
+
+
+if rankMPI == 0:
+	for i in rangue(ListSize):
+		print(List[1],OperSymb[List[3]],List[2],"=",List[4])
+
+sys.exit() 
